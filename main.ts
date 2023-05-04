680be3c9aa93c2504c4f6ebe34a70a5a15974a87
Voyage <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-05-04 21:32:44 +0300

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,21 @@
+BSD-3-Clause-Copyleft-5
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions, and the following additional clauses:
+
+   a. If you modify the software, you must release your modifications under the same license as the original software.
+   
+   b. If you distribute the software in binary form, you must include a copy of or make available the source code for the version of the software that you are distributing, and make it available under the same license as the original software.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and the following additional clauses:
+
+   a. If the binary form of the software includes a copy of or is derived from any part of the original software, you must make the source code of that part available under the same license as the original software.
+   
+   b. If you distribute the software in binary form, you must include a copy of or make available the source code for the version of the software that you are distributing, and make it available under the same license as the original software.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+4. You acknowledge that the software is experimental and is provided "AS IS" without warranty of any kind, including without limitation the warranties of merchantability, fitness for a particular purpose and non-infringement.
+
+5. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.
